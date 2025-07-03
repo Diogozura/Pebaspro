87ce00abb911f1315444374eac528d9a64ffc1de
Diogo Zura <65250673+Diogozura@users.noreply.github.com>
2025-07-03 16:14:33 -0300

    feat : Open Graph

--- pebaspro/app/layout.tsx
+++ pebaspro/app/layout.tsx
@@ -24,17 +24,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="keywords" content="pebaspro, profissionais, clientes, serviços, marketplace, conexão, agendamento, avaliações" />
+        <meta name="author" content="PebasPro Team" />
+
+        {/* Open Graph (Facebook, WhatsApp, LinkedIn) */}
+        <meta property="og:title" content="PebasPro - Encontre profissionais e serviços" />
+        <meta property="og:description" content="Conecte-se com profissionais qualificados ou encontre o serviço ideal. Tudo em um só lugar." />
+        <meta property="og:image" content="https://www.pebaspro.com.br/og-image.png" />
+        <meta property="og:url" content="https://www.pebaspro.com.br" />
+        <meta property="og:type" content="website" />
       </Head>
       <body>
         <ThemeRegistry>
           <Header />
           {children}
-           <CookiesAndPWA />
-           <MobileBottomNav/>
-          <Footer/>
+          <CookiesAndPWA />
+          <MobileBottomNav />
+          <Footer />
         </ThemeRegistry>
       </body>
     </html>
